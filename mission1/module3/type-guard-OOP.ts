234c09ce01364332a144de7ee29131b975209498
jahid433 <jahidhossen02@gmail.com>
2024-05-04 23:29:29 +0300

    created class constractor

--- mission1/module3/type-guard-OOP.ts
+++ mission1/module3/type-guard-OOP.ts
@@ -1,6 +1,38 @@
 {
     //instanceof guard
 
+    class Animal {
+        constructor(
+            name: string,
+            species: string
+        ) { }
+
+        makeSound() {
+            console.log('i am making sound');
+        }
+    }
+
+    class Dog {
+        constructor(
+            name: string,
+            species: string
+        ) { }
+
+        makeBark() {
+            console.log(`I am barking`);
+        }
+    }
+
+    class Cat {
+        constructor(
+            name: string,
+            species: string
+        ) { }
+
+        makeMeaw() {
+            console.log(`I am meawing`);
+        }
+    }
 
 
 
